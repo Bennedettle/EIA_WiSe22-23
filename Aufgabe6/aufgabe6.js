6c576ba78b2fcc24a111142486f4fc8e4a47a198
Bennedettle <benne.dettling@gmail.com>
2022-11-23 01:55:09 +0100

    Update aufgabe6.js

--- Aufgabe6/aufgabe6.js
+++ Aufgabe6/aufgabe6.js
@@ -1,32 +1,37 @@
 //# sourceMappingURL=aufgabe6.js.map
+const inhabitantsEU = " 446.8 ";
 const country1 = "Belgien"
 const inhabitants1 = " 11.6 ";
 console.log(country1 + " hat" + inhabitants1 + "Millionen Einwohner im Jahr 2022.")
-const overall1 = " 2.5%";
-console.log("Relativ zur Gesamtzahl der Einwohner der EU, beträgt die Einwohnerzahl in " + country1 + " im Jahr 2022:" + overall1 + ".")
-const comparison1 = " 9.4% "
-console.log("Für " + country1 + " hat sich die Einwohnerzahl im Jahr 2022 im Vergleich zu 2008 um" + comparison1 + "erhöht.")
+let overall1 = ((inhabitants1/inhabitantsEU)*100).toFixed(1);
+console.log("Relativ zur Gesamtzahl der Einwohner der EU, beträgt die Einwohnerzahl in " + country1 + " im Jahr 2022: " + overall1 + "%.")
+const inhabitants108 = " 10.7 "
+let comparison1 = ((1-inhabitants108/inhabitants1)*100).toFixed(1);
+console.log("Für " + country1 + " hat sich die Einwohnerzahl im Jahr 2022 im Vergleich zu 2008 um " + comparison1 + "% erhöht.")
 
 const country2 = "Deutschland"
-const inhabitants2 = " 84 ";
+const inhabitants2 = " 84.0 ";
 console.log(country2 + " hat" + inhabitants2 + "Millionen Einwohner im Jahr 2022.")
-const overall2 = " 18%";
-console.log("Relativ zur Gesamtzahl der Einwohner der EU, beträgt die Einwohnerzahl in " + country2 + " im Jahr 2022:" + overall2 + ".")
-const comparison2 = " 2% "
-console.log("Für " + country2 + " hat sich die Einwohnerzahl im Jahr 2022 im Vergleich zu 2008 um" + comparison2 + "erhöht.")
+let overall2 = ((inhabitants2/inhabitantsEU)*100).toFixed(1);
+console.log("Relativ zur Gesamtzahl der Einwohner der EU, beträgt die Einwohnerzahl in " + country2 + " im Jahr 2022: " + overall2 + "%.")
+const inhabitants208 = " 82.1 "
+let comparison2 = ((1-inhabitants208/inhabitants2)*100).toFixed(1);
+console.log("Für " + country2 + " hat sich die Einwohnerzahl im Jahr 2022 im Vergleich zu 2008 um " + comparison2 + "% erhöht.")
 
 const country3 = "Frankreich"
 const inhabitants3 = " 67.8 ";
 console.log(country3 + " hat" + inhabitants3 + "Millionen Einwohner im Jahr 2022.")
-const overall3 = " 15%";
-console.log("Relativ zur Gesamtzahl der Einwohner der EU, beträgt die Einwohnerzahl in " + country3 + " im Jahr 2022:" + overall3 + ".")
-const comparison3 = " 5% "
-console.log("Für " + country3 + " hat sich die Einwohnerzahl im Jahr 2022 im Vergleich zu 2008 um" + comparison3 + "erhöht.")
+let overall3 = ((inhabitants3/inhabitantsEU)*100).toFixed(1);
+console.log("Relativ zur Gesamtzahl der Einwohner der EU, beträgt die Einwohnerzahl in " + country3 + " im Jahr 2022: " + overall3 + "%.")
+const inhabitants308 = " 64.4 "
+let comparison3 = ((1-inhabitants308/inhabitants3)*100).toFixed(1);
+console.log("Für " + country3 + " hat sich die Einwohnerzahl im Jahr 2022 im Vergleich zu 2008 um " + comparison3 + "% erhöht.")
 
 const country4 = "Italien"
-const inhabitants4 = " 59 ";
+const inhabitants4 = " 59.0 ";
 console.log(country4 + " hat" + inhabitants4 + "Millionen Einwohner im Jahr 2022.")
-const overall4 = " 13%";
-console.log("Relativ zur Gesamtzahl der Einwohner der EU, beträgt die Einwohnerzahl in " + country4 + " im Jahr 2022:" + overall4 + ".")
-const comparison4 = " 1.6% "
-console.log("Für " + country4 + " hat sich die Einwohnerzahl im Jahr 2022 im Vergleich zu 2010 um" + comparison4 + "verringert.")
+let overall4 = ((inhabitants4/inhabitantsEU)*100).toFixed(1);
+console.log("Relativ zur Gesamtzahl der Einwohner der EU, beträgt die Einwohnerzahl in " + country4 + " im Jahr 2022: " + overall4 + "%.")
+const inhabitants408 = " 58.8 "
+let comparison4 = ((1-inhabitants408/inhabitants4)*100).toFixed(1);
+console.log("Für " + country4 + " hat sich die Einwohnerzahl im Jahr 2022 im Vergleich zu 2008 um " + comparison4 + "% erhöht.")
